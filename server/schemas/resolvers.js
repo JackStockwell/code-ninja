07319dd4d57bec61cd71a9f83b02d5c4b36b9543
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-09-06 20:31:59 +0100

    added createuser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,6 +13,24 @@ const resolvers = {
         categories: async () => {
             return await Category.find({})
         }
+    },
+    Mutation: {
+        // Creates a new user entry to the database.
+        createUser: async (parent, args) => {
+            // Create the user data from the args.
+            const newUser = await User.create(args)
+            // Error handler
+            if (!newUser) {
+                throw new AuthenticationError('Oops! Something went wrong')
+            }
+            // Creates a sign in token, allowing the user to navigate and give auth to the rest
+            // of the API.
+            const token = signToken(newUser)
+            
+            // Returns the token and
+            return { token, newUser }
+        },
+        
     }
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,15 +21,31 @@ const typeDefs = gql`
         categories: [Category]
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Category {
         _id: ID!
         name: String!
+    }
+
+    input userInput {
+        email: String!
+        password: String!
+        firstName: String!
+        lastName: String!
     }
 
     type Query {
         users: User
         jobs: Job
         categories: [Category]
+    }
+
+    type Mutation {
+        createUser(userData: userInput!): Auth
     }
 
 `;
