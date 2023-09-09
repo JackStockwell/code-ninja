a48c44d3d9805724c9c9cd30fa0d883cef722afa
JackStockwell <stockwell.jack@gmail.com>
2023-09-09 21:03:14 +0100

    changed file uploaded logic to an onsubmit event

--- client/src/components/UploadFile/index.js
+++ client/src/components/UploadFile/index.js
@@ -1,8 +1,9 @@
-import { gql, useMutation } from '@apollo/client';
+// React Imports
 import React, { useEffect, useState } from 'react';
 
-import { v4 as uuidv4 } from 'uuid'
-// import { SINGLE_UPLOAD } from '../../utils/mutations';
+// GraphQL imports
+import { useMutation } from '@apollo/client';
+import { SINGLE_UPLOAD } from '../../utils/mutations';
 
 
 const UploadFile = () => {   
@@ -14,79 +15,72 @@ const UploadFile = () => {
 
     const [fileData, setFileData] = useState(fileState)
 
-    // const [singleUpload, { error }] = useMutation(SINGLE_UPLOAD)
+    const [singleUpload, { error }] = useMutation(SINGLE_UPLOAD)
     
-    // const onFileChange = (e) => {
-    //     const { target } = e
-    //     console.log(target.files)
-    //     e.preventDefault();
+    const onFileChange = (e) => {
+
+        e.preventDefault();
         
-    //     setFileData({ ...fileData,
-    //         selectedFile: [e.target.files[0]],
-    //     })
+        const file = e.target.files[0]
 
-    //     console.log(target.validity.valid)
-
-    // }
-
-    // const onFileUpload = async () => {
-
-    //     const file = fileData.selectedFile[0]
-
-    //     console.log("Test")
+        console.log(file)
         
-    //     try {
-            
-    //         const { data } = await singleUpload({ 
-    //             variables: {file: fileData.selectedFile[0]}
-    //         })
+        setFileData({
+            ...fileData,
+            selectedFile: file,
+        })
 
-    //         console.log(data)
-    //     } catch (err) {
-    //         console.error(err)
-    //     }
-
-    // }
-    const SINGLE_UPLOAD = gql`
-    mutation Mutation($file: Upload!) {
-        singleUpload(file: $file) {
-            encoding
-            filename
-            mimetype
-            url
-        }
-    }
-    `;
-    const [mutate, { loading, error }] = useMutation(SINGLE_UPLOAD);
-
-    const onChange = async ({ target }) => {
-        const {
-          validity,
-          files: [file],
-        } = target;
-    
-        if (validity.valid) {
-
-            console.log(file)
-
-          try {
-            const { data } = await mutate(
-                { variables: { file }}
-            );
-            console.log(data);
-          } catch (error) {
-            console.error(error);
-          }
-        }
     };
+
+    const onFileUpload = async () => {
+
+        console.log(fileData)
+
+        const file = fileData.selectedFile
+
+        console.log(file)
+        
+        try {
+            
+            const { data } = await singleUpload({ 
+                variables: { file }
+            })
+
+            console.log(data)
+        } catch (err) {
+            console.error(err)
+        }
+
+    }
+
+    // const onChange = async ({ target }) => {
+    //     const {
+    //       validity,
+    //       files: [file],
+    //     } = target;
+    
+    //     if (validity.valid) {
+
+    //         console.log(file)
+
+    //       try {
+    //         const { data } = await mutate(
+    //             { variables: { file }}
+    //         );
+    //         console.log(data);
+    //       } catch (error) {
+    //         console.error(error);
+    //       }
+    //     }
+    // };
 
     return (
         <div>
             <h3>File Upload</h3>
             <p>Upload your resume here</p>
             <div>
-                <input required type='file' onChange={onChange} />
-                {/* <button onClick={onFileUpload}>Submit</button> */}
+                <input required type='file' onChange={onFileChange} />
+                <button onClick={onFileUpload}>Submit</button>
             </div>
         </div>
     )
