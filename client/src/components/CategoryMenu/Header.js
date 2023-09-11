bd9f0fb702cf617cb1be96ad7f2e050c1ebe423c
daleyjones <djones_15@hotmail.com>
2023-09-11 21:13:33 +0100

    added svg to components

--- client/src/components/CategoryMenu/Header.js
+++ client/src/components/CategoryMenu/Header.js
@@ -1,20 +1,20 @@
 // src/components/Header.js
-
 import React from 'react';
+import Logo from './Logo'; 
+
 
 function Header() {
   return (
     <header className="header">
+      <Logo xlinkHref="/images/logo.svg" /> 
       <nav>
         <ul>
           <li><a href="/">Home</a></li>
           <li><a href="/about">About us</a></li>
           <li><a href="/blog">Login</a></li>
-          {   }
+        
         </ul>
       </nav>
     </header>
   );
 }
-
-export default Header;
--- client/src/components/CategoryMenu/Logo.js
+++ client/src/components/CategoryMenu/Logo.js
@@ -1,14 +1,26 @@
-
+// src/components/Logo.js
 
 import React from 'react';
 
-function Logo() {
+const Logo = () => {
   return (
-    <div className="logo">
-      <img src="'.../images/3vSI9x-LogoMakr.png';" alt="Logo" />
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="100"
+        height="100"
+        viewBox="0 0 100 100"
+      >
+        <image
+          x="0"
+          y="0"
+          width="100"
+          height="100"
+          xlinkHref="/images/logo.svg"
+        />
+      </svg>
     </div>
   );
-}
+};
 
 export default Logo;
-
