9b6e67ca28e361616d1feaae34d3d628ecd4fe57
33hollie33 <hollybarnhurst@gmail.com>
2023-09-11 20:18:21 +0100

    update jpb

--- server/db/updatejob.js
+++ server/db/updatejob.js
@@ -10,3 +10,18 @@ async function updateJob(jobId, updatedData) {
   }
 }
 
+const jobIdToUpdate = 'job_id_to_update';
+const updatedJobData = {
+  title: 'New Job Title',
+  location: 'New Job Location',
+  salary: 75000.0,
+  description: 'Updated job description',
+};
+
+updateJob(jobIdToUpdate, updatedJobData)
+  .then(updatedJob => {
+    console.log('Job updated successfully:', updatedJob);
+  })
+  .catch(error => {
+    console.error('Error updating job:', error.message);
+  });
