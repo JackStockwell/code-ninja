a5937da22812525b4abb78d9f9733443bfe9e0d0
daleyjones <djones_15@hotmail.com>
2023-09-12 20:07:10 +0100

    fixed fortawesome error and error in app.js

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import { FaTwitter, FaLinkedin } from 'react-icons/fa'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faLinkedin } from '../fortawesome';
 import Header from '../components/Header/Header';
 import Logo from '../components/Logo/Logo';
 import './Homepage.css';
 
 const jobBlogPosts = [
-    {
-      title: "How to Ace Your Job Interview",
-      description: "Discover essential tips and strategies to help you excel in your next job interview, from preparation to follow-up.",
-    },
-    {
-      title: "Remote Work: Pros and Cons",
-      description: "Explore the advantages and disadvantages of remote work, and learn how to make the most of your remote job opportunities.",
-    },
-    {
-      title: "Resume Writing Tips for Job Seekers",
-      description: "Learn how to create a compelling resume that grabs the attention of employers and helps you stand out in the job market.",
-    },
-    {
-      title: "Navigating the Job Market in 2023",
-      description: "Stay up-to-date with the latest trends in the job market and gain insights into job search strategies for the current year.",
-    },
-  ];
+  {
+    title: "How to Ace Your Job Interview",
+    description: "Discover essential tips and strategies to help you excel in your next job interview, from preparation to follow-up.",
+  },
+  {
+    title: "Remote Work: Pros and Cons",
+    description: "Explore the advantages and disadvantages of remote work, and learn how to make the most of your remote job opportunities.",
+  },
+  {
+    title: "Resume Writing Tips for Job Seekers",
+    description: "Learn how to create a compelling resume that grabs the attention of employers and helps you stand out in the job market.",
+  },
+  {
+    title: "Navigating the Job Market in 2023",
+    description: "Stay up-to-date with the latest trends in the job market and gain insights into job search strategies for the current year.",
+  },
+];
+
 function Home() {
-  const currentYear = new Date().getFullYear(); 
+  const currentYear = new Date().getFullYear();
 
   return (
     <div className="home">
@@ -37,7 +39,6 @@ function Home() {
         <section className="container">
           <h2>Recent job blog Posts</h2>
           <ul>
-     
             {jobBlogPosts.map((post, index) => (
               <li key={index}>
                 <a href={`/blog/post-${index + 1}`}>{post.title}</a>
@@ -50,12 +51,12 @@ function Home() {
       <footer className="footer">
         <div className="footer-content">
           <div className="footer-icons">
-          <a href="https://twitter.com/your_twitter_profile">
-  <FontAwesomeIcon icon={FaTwitter} />
-</a>
-<a href="https://www.linkedin.com/in/your_linkedin_profile">
-  <FontAwesomeIcon icon={FaLinkedin} />
-</a>
+            <a href="https://twitter.com/your_twitter_profile">
+              <FontAwesomeIcon icon={faTwitter} /> {/* Use faTwitter */}
+            </a>
+            <a href="https://www.linkedin.com/in/your_linkedin_profile">
+              <FontAwesomeIcon icon={faLinkedin} /> {/* Use faLinkedin */}
+            </a>
           </div>
           <p>&copy; {currentYear} Git-Jobs</p>
         </div>
