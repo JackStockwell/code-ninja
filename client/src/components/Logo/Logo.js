8c3fa0584f762bdde2e24b8f75e33f9f25f2b5e9
daleyjones <djones_15@hotmail.com>
2023-09-12 19:48:25 +0100

    fixed logo error

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,14 +1,11 @@
-
-
 import React from 'react';
 
 function Logo() {
   return (
     <div className="logo">
-      <img src="'.../images/3vSI9x-LogoMakr.png';" alt="Logo" />
+      <img src="../images/3vSI9x-LogoMakr.png" alt="Logo" />
     </div>
   );
 }
 
 export default Logo;
-
