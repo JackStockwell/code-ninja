05816ea12ca5141937495379459f6712db24a1dd
daleyjones <djones_15@hotmail.com>
2023-09-17 20:53:51 +0100

    added user.css & and added more css to pages

--- client/src/components/LoggedProfiles/User.js
+++ client/src/components/LoggedProfiles/User.js
@@ -1,37 +1,37 @@
-import React from 'react'
+import React from 'react';
 import { GET_ME } from '../../utils/queries';
-import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
+import './User.css'; // Import your stylesheet
 
 const Profile = () => {
+  const { loading, data } = useQuery(GET_ME);
 
-    const { loading, data } = useQuery(GET_ME)
+  if (loading) {
+    return <h4 className="error-message">Loading...</h4>;
+  }
 
-    if (!data) {
-        console.log("Test")
-    }
+  const userData = data?.me || {};
 
-    if (loading) {
-        return <h4 style={{textAlign: 'center'}}>Loading...</h4>
-    }
+  return (
 
-    const userData = data?.me || []
+      <div className="profile-container">
+        <h2 className="profile-title">User Profile</h2>
+        {userData.firstName ? (
+          <div className="profile-data">
+            <p><span>First Name:</span> {userData.firstName}</p>
+            <p><span>Last Name:</span> {userData.lastName}</p>
+            <p><span>Email:</span> {userData.email}</p>
+            {/* Add more user data fields as needed */}
+          </div>
+        ) : (
+          <div className="error-message">
+            <p>Please log in to see your profile.</p>
+          </div>
+        )}
+        <p className="test-text">Test</p>
+      </div>
 
-    return (
-        <>
-            {userData?.firstName}
-            {!data.me && (
-                <>
-                    <p>Please Login in to see </p>
-                </>
-            )}
-            <p>Test</p>
-        </>
-    )
-}
+  );
+};
 
-
-
-
-export default Profile
+export default Profile;
