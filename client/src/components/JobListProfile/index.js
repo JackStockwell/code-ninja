292f2fe9a0c1fd07b8cd9c13008b0563e9dc8ba2
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-09-17 23:38:02 +0100

    added link function to joblistprofile

--- client/src/components/JobListProfile/index.js
+++ client/src/components/JobListProfile/index.js
@@ -1,10 +1,17 @@
 import React, {useEffect, useState }from 'react'
 
+import { useMediaPredicate } from 'react-media-hook';
+import EditorRender from '../TextEditor/Editor'
 import JobItem from '../JobItem';
+import { useNavigate } from 'react-router-dom';
 
 const JobListProfile = (userData) => {
 
+    const navigate = useNavigate()
+
     const [selectedJob, setSelectedJob] = useState({})
+
+    const handleMinWidth = useMediaPredicate("(min-width: 60em)")
 
     return (
         <>
@@ -12,23 +19,38 @@ const JobListProfile = (userData) => {
                 <div className='emp-job-list'>
                     {userData?.jobs.map((job) => {
                         return (
-                            <div className='job-card' onClick={() => setSelectedJob(job)}>
-                                <div key={job._id}>
-                                    <h4>{job.title}</h4>
-                                    <h5>{job.company.companyName}</h5>
-                                    <h5>{job.location}</h5>
+                            <div 
+                            key={job._id} 
+                            className='job-card' 
+                            value={job._id}
+                            onClick={() => {
+                                if(handleMinWidth) {
+                                    setSelectedJob(job)
+                                } else {
+                                    navigate(`/${job}/${job_id}`)
+                                }
+                            }}>
+                                <h4>{job.title}</h4>
+                                <h5>{job.company.companyName}</h5>
+                                <h5>{job.category.name}</h5>
+                                <div>
+                                    <EditorRender>
+                                        {job.description}
+                                    </EditorRender>
                                 </div>
                             </div>
                         )
                     })}
                 </div>
-                <div className='emp-job-viewer'>
-                    {selectedJob?.title && 
-                        <JobItem {...selectedJob} />
-                    }
-                </div>
+                {handleMinWidth && (
+                    <div className='emp-job-viewer'>
+                        {selectedJob?.title && 
+                            <JobItem {...selectedJob} />
+                        }
+                    </div>
+                )}
             </div>
-    </>
+        </>
     )
 }
 
