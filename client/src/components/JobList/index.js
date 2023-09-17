7a622bf4dec765613880395c36d7892b7b4f60c3
DiogoS77 <diogo.messi.18@hotmail.com>
2023-09-17 15:16:40 +0100

    removed not necessary h2 title

--- client/src/components/JobList/index.js
+++ client/src/components/JobList/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
-import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useJobContext } from "../../utils/GlobalState";
-import { QUERY_JOBS } from "../../utils/queries";
-import { UPDATE_JOBS } from "../../utils/actions";
-import { useQuery } from "@apollo/client";
+import {useEffect, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useJobContext} from "../../utils/GlobalState";
+import {QUERY_JOBS} from "../../utils/queries";
+import {UPDATE_JOBS} from "../../utils/actions";
+import {useQuery} from "@apollo/client";
 import "./JobList.css";
 
 import JobItem from "../JobItem";
@@ -12,84 +12,80 @@ import JobItem from "../JobItem";
 const PAGE_SIZE = 3;
 
 const JobList = () => {
-    // State import
-    const [state, dispatch] = useJobContext();
+  // State import
+  const [state, dispatch] = useJobContext();
 
-    // Get the current category from state.
-    let { currentCategory } = state;
+  // Get the current category from state.
+  let {currentCategory} = state;
 
-    // If there is no currentCat, turn the value to null. Allowing for an unfiltered response.
-    if (!currentCategory.length) {
-      currentCategory = null
+  // If there is no currentCat, turn the value to null. Allowing for an unfiltered response.
+  if (!currentCategory.length) {
+    currentCategory = null;
+  }
+
+  //
+  const [page, setPage] = useState(0);
+
+  const {data, loading, error} = useQuery(QUERY_JOBS, {
+    variables: {
+      limit: 5,
+      offset: page * PAGE_SIZE,
+      category: currentCategory,
+    },
+  });
+
+  function filterJobs() {
+    if (!currentCategory) {
+      return state.jobs;
     }
 
-    // 
-    const [page, setPage] = useState(0);
-  
-    const { data,  loading, error } = useQuery(QUERY_JOBS,
-      {
-        variables: {
-          limit: 5,
-          offset: page * PAGE_SIZE,
-          category: currentCategory
-        },
-      }
-    );
+    return state.jobs.filter((job) => job.category._id === currentCategory);
+  }
 
-    function filterJobs() {
-      if(!currentCategory) {
-        return state.jobs
-      }
-
-      return state.jobs.filter(
-        (job) => job.category._id === currentCategory
-      )
+  //Runs when data or currentCategory is updated.
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_JOBS,
+        jobs: data.jobs,
+      });
+      console.log(data.jobs);
     }
+  }, [data, loading, dispatch]);
 
-    //Runs when data or currentCategory is updated.
-    useEffect(() => {
-        if(data) {
-            dispatch({
-              type: UPDATE_JOBS,
-              jobs: data.jobs
-            })
-            console.log(data.jobs)
-        }
-    }, [data, loading, dispatch]);
-
-    return (
+  return (
+    <div>
+      {state.jobs?.length ? (
         <div>
-            <h2>Jobs:</h2>
-            {state.jobs?.length ? (
-              <div>
-                  {filterJobs().map((job, index) => {
-                  return <JobItem {...job} key={index} />
-                  })}
-              </div>
-            ) : (
-                <div><p>No Jobs</p></div>
-            )}
-<div className="button-container">
-  <button
-    disabled={!page}
-    onClick={() => setPage((prev) => prev - 1)}
-    className={`nav-button ${!page ? "disabled" : ""}`}
-  >
-    <FontAwesomeIcon icon="fa-solid fa-chevron-left" />
-  </button>
-  <button
-    disabled={state.jobs?.length < 5}
-    onClick={() => setPage((prev) => prev + 1)}
-    className={`nav-button ${state.jobs?.length < 5 ? "disabled" : ""}`}
-  >
-    <FontAwesomeIcon icon="fa-solid fa-chevron-right" />
-  </button>
-</div>
-
-
-            {loading ? <span>Loading...</span> : null}
+          {filterJobs().map((job, index) => {
+            return <JobItem {...job} key={index} />;
+          })}
         </div>
-    )
-}
+      ) : (
+        <div>
+          <p>No Jobs</p>
+        </div>
+      )}
+      <div className="button-container">
+        <button
+          disabled={!page}
+          onClick={() => setPage((prev) => prev - 1)}
+          className={`nav-button ${!page ? "disabled" : ""}`}
+        >
+          <FontAwesomeIcon icon="fa-solid fa-chevron-left" />
+        </button>
+        <button
+          disabled={state.jobs?.length < 5}
+          onClick={() => setPage((prev) => prev + 1)}
+          className={`nav-button ${state.jobs?.length < 5 ? "disabled" : ""}`}
+        >
+          <FontAwesomeIcon icon="fa-solid fa-chevron-right" />
+        </button>
+      </div>
+
+      {loading ? <span>Loading...</span> : null}
+    </div>
+  );
+};
 
 export default JobList;
