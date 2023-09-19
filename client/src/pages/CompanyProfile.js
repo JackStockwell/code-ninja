02dbf9f8967c46024c7428fcee3a4bebe380780b
DiogoS77 <diogo.messi.18@hotmail.com>
2023-09-19 01:23:04 +0100

    added style to company profile

--- client/src/pages/CompanyProfile.js
+++ client/src/pages/CompanyProfile.js
@@ -1,56 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import EmpProfile from '../components/LoggedProfiles/Employer'
-import { useParams } from 'react-router-dom'
+import React, {useEffect, useState} from "react";
+import EmpProfile from "../components/LoggedProfiles/Employer";
+import {useParams} from "react-router-dom";
 
 // Utils and Comps
-import Auth from '../utils/auth'
-import Employer from '../components/LoggedProfiles/Employer'
+import Auth from "../utils/auth";
+import Employer from "../components/LoggedProfiles/Employer";
 
 // Database Imports
-import { COMPANY_QUERY } from '../utils/queries'
-import { useQuery } from '@apollo/client'
+import {COMPANY_QUERY} from "../utils/queries";
+import {useQuery} from "@apollo/client";
 
-import JobListProfile from '../components/JobListProfile'
-
+import JobListProfile from "../components/JobListProfile";
+import "./styles/CompanyProfile.css";
 
 const CompanyProfile = () => {
+  const {id} = useParams();
 
-    const { id } = useParams()
+  const {loading, data} = useQuery(COMPANY_QUERY, {
+    variables: {getCompanyId: id},
+  });
 
-    const { loading, data } = useQuery(COMPANY_QUERY,
-        { variables: { getCompanyId: id } }
-    )
+  const empData = data?.getCompany || [];
 
-    const empData = data?.getCompany || []
+  console.log(empData);
 
-    console.log(empData)
+  return (
+    <div className="container">
+      {loading && <h4>Loading...</h4>}
+      {empData && (
+        <>
+          <div>
+            <h1 className="company-name">{empData.companyName}</h1>
+            <p className="about">{empData.about}</p>
+            <p className="email">{empData.email}</p>
+            <p className="location">{empData.location || "Location"}</p>
+          </div>
 
-    return (
-        <>  
-            {loading && (<h4>Loading...</h4>)}
-            {empData && (
-                <>
-                    <div>
-                        <h1>{empData.companyName}</h1>
-                        <p>{empData.about}</p>
-                        <p>{empData.email}</p>
-                        <p>{empData.location || "location"}</p>
-                    </div>
-
-                    <div style={{textAlign: 'center'}}>
-                        {empData.jobs?.length ? (
-                            <>
-                                <h3>Active Jobs</h3>
-                                <JobListProfile data={empData?.jobs} />
-                            </>
-                        ) : (
-                            <h3>No active jobs</h3>
-                        )}
-                    </div>
-                </>
+          <div className="center">
+            {empData.jobs?.length ? (
+              <>
+                <h3>Active Jobs</h3>
+                <ul className="job-list">
+                  <JobListProfile data={empData?.jobs} />
+                </ul>
+              </>
+            ) : (
+              <h3>No active jobs</h3>
             )}
+          </div>
         </>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default CompanyProfile
+export default CompanyProfile;
